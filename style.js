91c93e20d4b87ca0ced35fe5442bd3833fb9d45e
ahmedz182 <ahmedmughal3182@gmail.com>
2023-01-09 12:52:50 +0500

    new buttons

--- style.js
+++ style.js
@@ -1,7 +1,21 @@
+$(document).ready(function () {
+
+    $(".changetext").hide();
+    $("#btn2").click(function () {
+
+        $(".changetext").show();
+    });
+    $("#btn3").click(function () {
+
+        $(".changetext").show();
+    });
+
+});
 
 
-var btn = document.getElementById('btn').addEventListener("click", counters);
-
+var btn1 = document.getElementById('btn').addEventListener("click", counters);
+var btn2 = document.getElementById('btn2').addEventListener("click", lowercase);
+var btn3 = document.getElementById('btn3').addEventListener("click", uppercase);
 
 function counters() {
 
@@ -14,3 +28,28 @@ function counters() {
 
 }
 
+
+function lowercase() {
+
+    let txt = document.getElementById('texts').value
+    let a = document.getElementById('newtext').innerText = txt.toLowerCase();
+
+
+}
+
+function uppercase() {
+    let txt = document.getElementById('texts').value
+    let a = document.getElementById('newtext').innerText = txt.toUpperCase();
+
+}
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    //   document.getElementById("myDiv").style.display = "block";
+}
