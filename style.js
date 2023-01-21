c167b7aedd422e39691e39a48c3dbd9e0ab2b01c
ahmedz182 <ahmedmughal3182@gmail.com>
2023-01-21 09:58:11 +0500

    bug fixed

--- style.js
+++ style.js
@@ -13,14 +13,12 @@ $(document).ready(function () {
 
 
     $("#btn2").click(function () {
-        $(".textcount").hide();
         $(".changetext").hide();
         $(".loader").show().delay(2000).hide("slow");
         $(".changetext").delay(2000).show(300);
 
     });
     $("#btn3").click(function () {
-        $(".textcount").hide();
         $(".changetext").hide();
         $(".loader").show().delay(2000).hide("slow");
         $(".changetext").delay(2000).show(300);
@@ -30,14 +28,13 @@ $(document).ready(function () {
 
 
 var btn1 = document.getElementById('btn').addEventListener("click", counters);
-
-// var btn1 = document.getElementById('btn')
 var btn2 = document.getElementById('btn2').addEventListener("click", lowercase);
 var btn3 = document.getElementById('btn3').addEventListener("click", uppercase);
 
 
-function counters() {
 
+//FUnction to count the characters and words
+function counters() {
     let txt = document.getElementById('texts')
     let newtxt = txt.value
     document.getElementById('chr').innerText = "Total charcters : " + newtxt.length
@@ -48,14 +45,14 @@ function counters() {
 
 }
 
-
+//function to turn any text into lowerCase
 function lowercase() {
-
     let txt = document.getElementById('texts').value
     let a = document.getElementById('newtext').innerText = txt.toLowerCase();
 
 
 }
+//function to turn any text into UpperCase
 
 function uppercase() {
     let txt = document.getElementById('texts').value
@@ -69,7 +66,7 @@ function myFunction() {
     myVar = setTimeout(showPage, 3000);
 }
 
+//animation hide function
 function showPage() {
     document.getElementById("loader").style.display = "none";
-    //   document.getElementById("myDiv").style.display = "block";
 }
